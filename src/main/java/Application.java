d9403ccbbbc6afb0b3a1f6915e69d2a72d6da3f6
ElnathanAu <elnathanau1@gmail.com>
2020-06-07 19:41:41 -0500

    Test commit branch

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,5 +2,6 @@ public class Application {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Welcome!");
+        System.out.println("This is my new text");
     }
 }
