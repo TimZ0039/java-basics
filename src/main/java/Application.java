ceb1a058ea8cb34da8aa6c4778fba23e5d763d12
Tim Zhang <mrzhangtim@gmail.com>
2020-06-11 20:17:44 -0700

    polynomial function

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,18 @@
 public class Application {
     public static void main(String[] args){
-        int a = 10;
-        int b = square(a);
-        System.out.println(b);
+       int x = 1;
+       int y = polynomial(x);
+       System.out.println(y);
     }
 
     public static int square(int a) {
         return a * a;
     }
+
+    public static int polynomial(int x){
+        int y = square(x)+2*x+1;
+        return y;
+    }
+
+
 }
