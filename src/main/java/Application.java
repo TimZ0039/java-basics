806e28c7788e7cedbfbc3e4fdde29302c81b26fc
Tim Zhang <mrzhangtim@gmail.com>
2020-06-14 17:21:15 -0700

    message

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,15 @@
 public class Application {
     public static void main(String[] args){
-        int a = 10;
-        int b = square(a);
-        System.out.println(b);
+        // declare a variable
+        String a = "Tim";
+
+        //
+        welcome(a);
+
     }
 
-    public static int square(int a) {
-        return a * a;
+    public static void welcome(String name) {
+        // print out "Hello" and the name
     }
+
 }
