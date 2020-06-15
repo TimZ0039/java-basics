dc087c92487239cbc949ff9736bb93918137cc9e
Tim Zhang <mrzhangtim@gmail.com>
2020-06-14 17:38:01 -0700

    message

--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PriceCalculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Pleas enter two numbers:");
+        System.out.println("Please enter two numbers:");
         double firstItem=in.nextDouble();
         double secondItem=in.nextDouble();
         System.out.println("The items I received:"+firstItem+","+secondItem);
