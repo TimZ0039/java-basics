e8e02a44fc4eff9aa32fcb6e3d4877967dee7738
TimZ0039 <66567612+TimZ0039@users.noreply.github.com>
2020-06-21 20:14:32 -0700

    Price calculator (#14)
    * message
    
    * message
    
    * if else if else

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,14 @@
 public class Application {
     public static void main(String[] args){
-       int x = 1;
-       int y = polynomial(x);
-       System.out.println(y);
+
     }
 
     public static void welcome(String name) {
         // print out "Hello" and the name
     }
 
-    public static int polynomial(int x){
-        int y = square(x)+2*x+1;
-        return y;
+
     }
 
 
-}
+
--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -6,7 +6,17 @@ public class PriceCalculator {
         System.out.println("Please enter two numbers:");
         double firstItem=in.nextDouble();
         double secondItem=in.nextDouble();
+        double price=firstItem+secondItem;
         System.out.println("The items I received:"+firstItem+","+secondItem);
+        if (price > 50){
+            System.out.println("$" + price + "That is too expensive");
+        }
+        else if (price < 10){
+            System.out.println("$" + price + "That is a great deal.");
+        }
+        else
+            System.out.println("$" + price);
+
     }
 }
 
